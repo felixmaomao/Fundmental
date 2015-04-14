85c36e92ba0a8dbc1301d6d5cd2fba7213c2b7c0
Jason <swfightforyl@163.com>
2015-04-15 06:20:43 +0800

    y

--- CSharp坦克轰炸/基础知识轰炸/抽象类.cs
+++ CSharp坦克轰炸/基础知识轰炸/抽象类.cs
@@ -5,6 +5,11 @@ using System.Text;
 
 namespace 基础知识轰炸
 {
+    //总结一下：抽象类 和接口都是类的抽象，接口更关注从行为方面来抽象，而在语法上 接口的内容必须都是没有实现的，完全等待后面的类来实现的
+    //而在这一点上，抽象类则像是一个没有完全实现的类，他想所有普通类一样，可以拥有字段，属性，以及方法，这些都是可以有具体实现体的，
+    //但就这样的话，他和普通类的差别只在于他不可以通过new实例化，而他的真正不同在于 他应该有abstract修饰的部分，这部分没有具体实现，必须由
+    //由后面的实现类来具体实现。
+    //而virtual关键字本不该属于这里的讨论范畴，他修饰的方法和普通方法几乎一样，都是有着方法体的，但是被virtual修饰的可以被子类重写。
     class 抽象类
     {
         public static void Main(string[] args)
@@ -16,14 +21,23 @@ namespace 基础知识轰炸
             Console.WriteLine(c.Age);
             Console.WriteLine(c.Hobby);
             Console.WriteLine("--------");
+            c.fight();
             JiangSuPerson j = new JiangSuPerson();
             j.Name = "zhangxiaomao";
             j.Age = 24;
             j.Run();
             j.Speak();
+            j.fight();
+            j.sing();
             Console.WriteLine(j.Name);
             Console.WriteLine(j.Age);
             Console.WriteLine(j.Hobby);
+            Console.WriteLine("----------");
+            HenanPerson h = new HenanPerson();
+            h.Name = "zhanglinyi";
+            h.Speak();
+            h.fight();
+            h.sing();
             Console.ReadKey();
         }
     }
@@ -63,6 +77,12 @@ namespace 基础知识轰炸
         {
             Console.WriteLine("Running ~~");
         }
+        //能够被子类重写的 只有 abstract 或virtual修饰的，
+        //不同在于，abstract一定要被重写，而virtual则是可以被重写 但不一定非要重写，还有就是virtual要有方法体。
+        public virtual void fight()
+        {
+            Console.WriteLine("fighting");
+        }
     }
     public class ChinesePerson : Person
     {
@@ -90,12 +110,32 @@ namespace 基础知识轰炸
             {
             }
         }
+        public override void fight()
+        {
+            base.fight();
+            Console.WriteLine("chinese gongfu fighting");
+        }
+        public virtual void sing()
+        {
+            Console.WriteLine("用中文唱歌");
+        }
     }
 
     //继承自中国人,但是没有任何重写
     public class JiangSuPerson:ChinesePerson
     {
         
+    }
+    public class HenanPerson : ChinesePerson
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("河南人说河南方言");
+        }
+        public override void sing()
+        {
+            Console.WriteLine("用河南话唱歌");
+        }
     }
 
 }
