c557f3e21e6cb9801e7c076fc0cda5ef181defa9
Jason <swfightforyl@163.com>
2015-04-27 19:16:08 +0800

    2

---
+++ CSharp坦克轰炸/基础知识轰炸/工厂模式.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace 基础知识轰炸
+//{
+//    //这个例子我们将阐释 什么是工厂模式，以及为什么要使用工厂模式
+
+//    class 工厂模式
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CarFactory fac = new CarFactory();
+//            Car c = fac.CreateCar("BMW",30);
+            
+//        }
+//    }
+
+//    class Car
+//    {
+//        private string Logo;
+//        private int Money;
+//        public Car(string logo,int money)
+//        {
+//            this.Logo = logo;
+//            this.Money = money;
+//        }
+//    }
+//    class CarFactory
+//    {
+//        public Car CreateCar(string logo,int mon)
+//        {
+//            return new Car(logo,mon);
+//        }
+//    }
+//}
