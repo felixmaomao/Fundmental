e807a48f085e46e1686ba9ba37931509b628d3b2
Jason <swfightforyl@163.com>
2015-05-21 15:15:36 +0800

    4

---
+++ CSharp坦克轰炸/基础知识轰炸/字典以及序列化等常用.cs
@@ -0,0 +1,140 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+//namespace 基础知识轰炸
+//{
+//    //相当好的一个例子
+//    class 字典以及序列化等常用
+//    {
+//        public static void Main(string[] args)
+//        {
+//            School s = new School();
+//            s.EnsureInitialized();
+//            s.fillCache();
+//            HashSet<string> places=new HashSet<string>();
+//            places.Add("江苏");
+//            places.Add("河南");
+//            ICollection<Person> find=s.FindPerson("shenwei",places,IsComputer);
+//            foreach(Person p in find)
+//            {
+//                Console.WriteLine(p.Name);
+//                Console.WriteLine(p.Province);
+//                Console.WriteLine(p.sex);
+//                Console.WriteLine(p.Depart);
+//            }
+//            Console.ReadKey();
+
+//        }
+//        public static bool IsMan(Person p)
+//        {
+//            return p.sex == "男";
+//        }
+//        public static bool IsComputer(Person p)
+//        {
+//            return p.Depart == "计算机";
+//        }
+//    }
+
+//    public  class Person
+//    {
+//        public string Name;
+//        public string Province;
+//        public string sex;
+//        public string Depart;
+//        public Person(string name,string pro,string sex,string depa)
+//        {
+//            this.Name = name;
+//            this.Province = pro;
+//            this.sex = sex;
+//            this.Depart = depa;
+//        }
+//    }
+//    public class School
+//    {
+//        public List<Person> students = new List<Person>();
+//        public void EnsureInitialized()
+//        {
+//            students.Add(new Person("shenwei","江苏","男","计算机"));
+//            students.Add(new Person("shenwei", "河南", "男", "建筑"));
+//            students.Add(new Person("shenwei", "江苏", "女", "建筑"));
+//            students.Add(new Person("zhangxiaomao", "江苏", "女", "纺织"));
+//            students.Add(new Person("zhanglinyi", "河南", "男", "计算机"));
+//            students.Add(new Person("zhangjiahao", "河南", "男", "数学"));
+//            students.Add(new Person("zhanglinyi", "江苏", "女", "电子"));
+//            students.Add(new Person("shenjiaju", "江苏", "男", "英语"));
+//            students.Add(new Person("zhanghan", "河南", "男", "机械"));
+//        }
+//        public Dictionary<string, ILookup<string, Person>> _cache = new Dictionary<string, ILookup<string, Person>>();
+//        public void fillCache()
+//        {
+//            var groupbyname = students.GroupBy(s=>s.Name);
+//            _cache = groupbyname.ToDictionary(
+//                g=>g.Key,
+//                g=>g.ToLookup(p=>p.Province)
+//                );
+//        }
+//        //相当复杂的寻人方法。
+//        public ICollection<Person> FindPerson(string name,HashSet<string> provinces,Predicate<Person> predict)
+//        {
+//            HashSet<Person> matches = new HashSet<Person>();
+//            ILookup<string, Person> prolookup;
+//            if(_cache.TryGetValue(name,out prolookup))
+//            {
+//                if (provinces != null)
+//                {
+//                    foreach (string requestedProvince in provinces)
+//                    {
+//                        foreach (var pro in prolookup)
+//                        {
+//                            if (pro.Key.Equals(requestedProvince))
+//                            {
+//                                if (predict == null)
+//                                {
+//                                    matches.UnionWith(pro);
+//                                }
+//                                else
+//                                { 
+//                                    HashSet<Person> temp=new HashSet<Person>();
+//                                    foreach(var p in pro)
+//                                    {
+//                                        if (predict(p))
+//                                        {
+//                                            temp.Add(p);
+//                                        }
+//                                    }
+//                                    matches.UnionWith(temp);
+//                                }
+                                
+//                            }
+//                        }
+//                    }
+//                }
+//                else
+//                {
+//                    foreach (var pro in prolookup)
+//                    {
+//                        if (predict == null)
+//                        {
+//                            matches.UnionWith(pro);
+//                        }
+//                        else
+//                        {
+//                            HashSet<Person> temp = new HashSet<Person>();
+//                            foreach (var p in pro)
+//                            {
+//                                if (predict(p))
+//                                {
+//                                    temp.Add(p);
+//                                }
+//                            }
+//                            matches.UnionWith(temp);
+//                        }                       
+//                    }
+//                }
+//            }
+//            return matches;
+//        }
+//    }
+//}
--- CSharp坦克轰炸/基础知识轰炸/模拟一下二级字典的写法.cs
+++ CSharp坦克轰炸/基础知识轰炸/模拟一下二级字典的写法.cs
@@ -1,86 +1,86 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
 
-namespace 基础知识轰炸
-{
-    class 模拟一下二级字典的写法
-    {
-        public static Dictionary<string, ILookup<string, Person>> _cache = new Dictionary<string, ILookup<string, Person>>();
-        public static void Main(string[] args)
-        {
-            List<Person> persons = new List<Person>();
-            persons.Add(new Person("shenwei", "jiangsu"));
-            persons.Add(new Person("shenwei", "henan"));
-            persons.Add(new Person("shenwei", "beijing"));
-            persons.Add(new Person("zhangxiaomao", "jiangsu"));
-            persons.Add(new Person("zhangxiaomao", "shanghai"));
-            persons.Add(new Person("zhangliyi", "henan"));
-            persons.Add(new Person("daxiong", "shanghai"));
-            persons.Add(new Person("xiaoxiong","shanghai"));
+//namespace 基础知识轰炸
+//{
+//    class 模拟一下二级字典的写法
+//    {
+//        public static Dictionary<string, ILookup<string, Person>> _cache = new Dictionary<string, ILookup<string, Person>>();
+//        public static void Main(string[] args)
+//        {
+//            List<Person> persons = new List<Person>();
+//            persons.Add(new Person("shenwei", "jiangsu"));
+//            persons.Add(new Person("shenwei", "henan"));
+//            persons.Add(new Person("shenwei", "beijing"));
+//            persons.Add(new Person("zhangxiaomao", "jiangsu"));
+//            persons.Add(new Person("zhangxiaomao", "shanghai"));
+//            persons.Add(new Person("zhangliyi", "henan"));
+//            persons.Add(new Person("daxiong", "shanghai"));
+//            persons.Add(new Person("xiaoxiong","shanghai"));
 
-            var groupbyname = persons.GroupBy(p=>p.name);
-            _cache = groupbyname.ToDictionary(
-                      g=>g.Key,
-                      g=>g.ToLookup(p=>p.address)
-                );
+//            var groupbyname = persons.GroupBy(p=>p.name);
+//            _cache = groupbyname.ToDictionary(
+//                      g=>g.Key,
+//                      g=>g.ToLookup(p=>p.address)
+//                );
 
-            //下面使用找人方法,寻找 河南和江苏的shenwei
-            HashSet<string> addresses = new HashSet<string>();
-            addresses.Add("jiangsu");
-            addresses.Add("henan");
-            ICollection<Person> find = GetPerson("shenwei",addresses);
-            foreach(Person p in find)
-            {
-                Console.WriteLine(p.name);
-                Console.WriteLine(p.address);
-            }
-            //这样子分成字典有什么好处呢？你可能会愚蠢的问。很明显，如果不分字典进行查找的话，那么会需要完全遍历，当数据量大的时候会相当耗时。
-            //这样分成字典之后，查找起来就像显示生活中查字典一样 相当轻便了。 
-            Console.ReadKey();
-        }
-        public static ICollection<Person> GetPerson(string name,HashSet<string> addresses)
-        {
-            HashSet<Person> matches = new HashSet<Person>();
-            ILookup<string, Person> addrlookup;
-            if(_cache.TryGetValue(name,out addrlookup))
-            {
-                if (addresses != null)
-                {
-                    foreach (string requestedaddress in addresses)
-                    {
-                        foreach (var groupaddress in addrlookup)
-                        {
-                            if (requestedaddress.Equals(groupaddress.Key))
-                            {
-                                matches.UnionWith(groupaddress);
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    foreach(var groupaddress in addrlookup)
-                    {
-                        matches.UnionWith(groupaddress);
-                    }
-                }
-            }
-            return matches;
-        }
+//            //下面使用找人方法,寻找 河南和江苏的shenwei
+//            HashSet<string> addresses = new HashSet<string>();
+//            addresses.Add("jiangsu");
+//            addresses.Add("henan");
+//            ICollection<Person> find = GetPerson("shenwei",addresses);
+//            foreach(Person p in find)
+//            {
+//                Console.WriteLine(p.name);
+//                Console.WriteLine(p.address);
+//            }
+//            //这样子分成字典有什么好处呢？你可能会愚蠢的问。很明显，如果不分字典进行查找的话，那么会需要完全遍历，当数据量大的时候会相当耗时。
+//            //这样分成字典之后，查找起来就像显示生活中查字典一样 相当轻便了。 
+//            Console.ReadKey();
+//        }
+//        public static ICollection<Person> GetPerson(string name,HashSet<string> addresses)
+//        {
+//            HashSet<Person> matches = new HashSet<Person>();
+//            ILookup<string, Person> addrlookup;
+//            if(_cache.TryGetValue(name,out addrlookup))
+//            {
+//                if (addresses != null)
+//                {
+//                    foreach (string requestedaddress in addresses)
+//                    {
+//                        foreach (var groupaddress in addrlookup)
+//                        {
+//                            if (requestedaddress.Equals(groupaddress.Key))
+//                            {
+//                                matches.UnionWith(groupaddress);
+//                            }
+//                        }
+//                    }
+//                }
+//                else
+//                {
+//                    foreach(var groupaddress in addrlookup)
+//                    {
+//                        matches.UnionWith(groupaddress);
+//                    }
+//                }
+//            }
+//            return matches;
+//        }
 
-    }
-    class Person
-    {
-        public string name;
-        public string address;
+//    }
+//    class Person
+//    {
+//        public string name;
+//        public string address;
        
-        public Person(string name, string address)
-        {
-            this.name = name;
-            this.address = address;
+//        public Person(string name, string address)
+//        {
+//            this.name = name;
+//            this.address = address;
           
-        }
-    }
-}
+//        }
+//    }
+//}
